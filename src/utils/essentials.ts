b4b6f2516a7657a46be544b655d4bcc95219d54a
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-06-25 13:51:14 -0400

    updated .gitignore

--- src/utils/essentials.ts
+++
@@ -1,9 +0,0 @@
-function delay(time: number) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-
-export async function sleep(time: number) {
-    console.log('start timer');
-    await (() => new Promise(resolve => setTimeout(resolve, time)))();
-    console.log('end timer');
-}  
