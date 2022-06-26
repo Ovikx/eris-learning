b0a2a8baee0ffe9b2bcfb7f79e700a79734b033d
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-06-26 04:05:00 -0400

    basic slash commands

--- src/appCommands.ts
+++ src/appCommands.ts
@@ -1,23 +1,78 @@
 import Eris from 'eris';
+//const Eris = require('eris');
 import { sleep } from './utils/essentials';
 const dotenv = require('dotenv');
-dotenv.config()
+dotenv.config();
 
 const token: string = process.env.BOT_TOKEN ?? '';
+const guildID: string = process.env.TESTING_GUILD_ID ?? '';
 const bot: Eris.Client = new Eris.Client(token, {
-    intents: ['guildMessages'],
+    intents: [],
     messageLimit: 100
 })
 
 bot.on('ready', async () => {
     console.log(`Logged in as ${String(bot.user)}`);
-    const commands = await bot.getCommands();
+    const commands = await bot.getGuildCommands(guildID);
+    
+
+    console.log('adding commands');
+    console.log(commands);
+    await bot.bulkEditGuildCommands(
+        guildID ?? '',
+        [
+            {
+                name: 'print_repeat',
+                description: 'Prints hello word `n` number of times!!!',
+                type: Eris.Constants.ApplicationCommandTypes.CHAT_INPUT,
+                options: [
+                    {
+                        name: 'string',
+                        description: 'the string to repeat',
+                        type: Eris.Constants.ApplicationCommandOptionTypes.STRING,
+                        required: true,
+                    },
+                    {
+                        name: 'repeat',
+                        description: 'how many times the string should be repeated',
+                        type: Eris.Constants.ApplicationCommandOptionTypes.INTEGER,
+                        required: true,
+                        autocomplete: true,
+                        min_value: 1,
+                        max_value: 100
+
+                    }
+                ] as any[]
+            }
+        ]
+    );
+    console.log(commands);
+    
 });
 
-bot.on('messageCreate', async (msg) => {
-    if (msg.content === 'hello') {
-        bot.createMessage(msg.channel.id, 'AYOO');
-    }
+bot.on('error', (err) => {
+    console.log(err);
 })
+
+bot.on("interactionCreate", (interaction) => {
+    if(interaction instanceof Eris.CommandInteraction) {
+        switch(interaction.data.name) {
+            case 'print_repeat':
+                let arr = [];
+                for (let i = 0; i < (interaction.data.options![1] as any).value; i++) {
+                    arr.push((interaction.data.options![0] as any).value);
+                }
+
+                interaction.createMessage(arr.join('\n'));
+                break;
+            case "test_delete_command":
+                interaction.createMessage("interaction recieved");
+                return bot.deleteCommand(interaction.data.id);
+            default: {
+                return interaction.createMessage("interaction recieved");
+            }
+        }
+    }
+});
 
 bot.connect();
--- src/oldMethod.ts
+++ src/oldMethod.ts
@@ -1,4 +1,5 @@
 // Invite: https://discord.com/api/oauth2/authorize?client_id=990291850051092491&scope=bot&permissions=8
+// https://discord.com/api/oauth2/authorize?response_type=code&client_id=157730590492196864&scope=identify%20guilds.join&state=15773059ghq9183habn&redirect_uri=https%3A%2F%2Fnicememe.website&prompt=consent
 
 import Eris from 'eris';
 import { sleep } from './utils/essentials';
