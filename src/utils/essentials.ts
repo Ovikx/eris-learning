37482ce56a18065d8a33adcd78fc095f5cdc067d
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-06-27 11:49:33 -0400

    removed delay function

--- src/utils/essentials.ts
+++ src/utils/essentials.ts
@@ -1,7 +1,3 @@
-function delay(time: number) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-
 export async function sleep(time: number) {
     console.log('start timer');
     await (() => new Promise(resolve => setTimeout(resolve, time)))();
