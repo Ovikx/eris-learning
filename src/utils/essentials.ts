b4e33dfd0acd5da3c8d2342991e32d522640a571
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-06-29 00:31:40 -0400

    added randrange function to essentials

--- src/utils/essentials.ts
+++ src/utils/essentials.ts
@@ -1,3 +1,7 @@
 export async function sleep(ms: number) {
     await (() => new Promise(resolve => setTimeout(resolve, ms)))();
 }
+
+export function randRange(max: number, min: number): number {
+    return Math.random() * (max-min) + min;
+}
